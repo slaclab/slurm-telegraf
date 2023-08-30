e1e302254b76d916ff58dde6cfaabf8973bbf596
Jhonatan Amado <jhonatanamadov@gmail.com>
2023-08-30 12:13:29 -0500

    Fix blank_space_issue

--- slurm_sinfo_json.py
+++ slurm_sinfo_json.py
@@ -34,8 +34,8 @@ def influxDBLineProtocol(measurement_name,input_dict):
     """
 
     input_dict = {key: None if value == '' else value for key, value in input_dict.items()}
-    formatted_tags = [f"{key}={value}" if value is not None else f"{key}=\\\"\\\"" for key, value in input_dict.items() if key in input_dict['idb_tags']]
-    formatted_fields = [f"{key}={value}" if value is not None else f"{key}=0" for key, value in input_dict.items() if key in input_dict['idb_fields']]
+    formatted_tags = [f"{key}='{value}'" if value is not None and isinstance(value, str) else f"{key}=\\\"\\\"" for key, value in input_dict.items() if key in input_dict['idb_tags']]
+    formatted_fields = [f"{key}='{value}'" if value is not None and isinstance(value, str) else f"{key}=0" for key, value in input_dict.items() if key in input_dict['idb_fields']]
 
     formatted_output =','.join([measurement_name] + [','.join(formatted_tags)])
     formatted_output = formatted_output+' '+','.join(formatted_fields)+' '+str(input_dict['timestamp'])
